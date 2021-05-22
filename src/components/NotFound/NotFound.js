a7f9303697f9cb1f1a9171818073907f395ce5a6
Alena Zavadskaya <alena1yakusheva@gmail.com>
2021-05-22 13:18:09 +0300

    create NotFound page

---
+++ src/components/NotFound/NotFound.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./NotFound.css";
+
+function NotFound() {
+  return (
+    <section className="not-found">
+			<div className="not-found__icon" />
+			<p className="not-found__text">User not found</p>
+    </section>
+  );
+}
+
+export default NotFound;
