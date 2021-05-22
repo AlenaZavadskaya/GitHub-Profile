bf4d5ebff7abe97ffbaad8853295ea4fde212753
Alena Zavadskaya <alena1yakusheva@gmail.com>
2021-05-22 13:17:38 +0300

    create initialState pege

---
+++ src/components/InitialState/InitialState.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./InitialState.css";
+
+function InitialState() {
+  return (
+    <section className="initial">
+			<div className="initial__search" />
+			<p className="initial__text">Start with searching a GitHub user</p>
+    </section>
+  );
+}
+
+export default InitialState;
